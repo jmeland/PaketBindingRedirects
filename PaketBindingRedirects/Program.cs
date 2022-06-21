f56320ddf583a204713161171d267ba2beb76ac6
Daniel Melanson <jmeland@jci.com>
2022-06-21 15:41:58 -0400

    Add project files.

---
+++ PaketBindingRedirects/Program.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace PaketBindingRedirects
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] test = new int[] { 1, 2, 3 };
+            Console.WriteLine(test);
+            Console.WriteLine(JsonConvert.SerializeObject(test));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
